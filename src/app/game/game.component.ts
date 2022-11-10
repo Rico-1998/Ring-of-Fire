0f24dc29f38035615c61bfc016b8dc237d5d62a0
Rico-1998 <rico.dg40@gmail.com>
2022-11-10 19:46:13 +0100

    second commit

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game: Game;
+  currentCard: string = '';
+
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.newGame()
+  }
+
+  newGame() {
+    this.game = new Game();
+
+  }
+
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      console.log('new card' + this.currentCard);
+      console.log('Game is', this.game);
+
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
+    });
+  }
+
+}
